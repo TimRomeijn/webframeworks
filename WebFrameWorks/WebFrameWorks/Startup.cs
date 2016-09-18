02cba5fda34ef3cefcd866c21587a14de51baf86
TimRomeijn <tim28@live.nl>
2016-09-18 22:08:13 +0200

    Basic Project Setup ASP.net
    Basic Project Setup ASP.net

---
+++ WebFrameWorks/WebFrameWorks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebFrameWorks.Startup))]
+namespace WebFrameWorks
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
