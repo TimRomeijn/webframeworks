ea69f5c4b68f19dab62f1ece00013217b3d634bd
TimRomeijn <tim28@live.nl>
2016-09-22 12:24:02 +0200

    Project Setup 2.0
    Project Setup 2.0
    en eerste model aangemaakt.

---
+++ webframeworks-MusicRator/webframeworks-MusicRator/Models/albumModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webframeworks_MusicRator.Models
+{
+    public class AlbumModel
+    {
+        public string AlbumName { get; set; }
+        public string Artist { get; set; }
+        public int ReleaseYear { get; set; }
+
+    }
+}
---
+++ webframeworks-MusicRator/webframeworks-MusicRator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(webframeworks_MusicRator.Startup))]
+namespace webframeworks_MusicRator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
